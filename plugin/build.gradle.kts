b9716d1f58d1259ede4eccaf439c6c5d3b3a902b
Tony Robalik <tony.robalik@gmail.com>
2026-09-24 17:08:06 -0700

    chore: prepare next development version.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "com.autonomousapps.platform-builder"
-version = "0.3"
+version = "0.4-SNAPSHOT"
 
 val isSnapshot: Boolean = version.toString().endsWith("SNAPSHOT")
 val isRelease: Boolean = !isSnapshot
